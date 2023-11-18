5783cb7bba3c04be5c345e88acf355daacde3fe2
pvlov <ge84nun@mytum.de>
2023-11-18 12:05:57 +0100

    some more init stuff

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 mod server;
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{web, App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Listening on 127.0.0.1:8080");
-    HttpServer::new(|| App::new().service(server::index).service(server::hello))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(server::index)
+            .service(server::get_craftsmen_data)
+            .service(
+                web::resource("/craftman/{craftman_id}")
+                    .route(web::patch().to(server::update_craftsman)),
+            )
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
--- src/server.rs
+++ src/server.rs
@@ -1,11 +1,41 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, web, HttpResponse, Responder};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct PatchRequest {
+    max_driving_distance: Option<i64>,
+    procile_picture_score: Option<i64>,
+    profile_description_score: Option<i64>,
+}
 
 #[get("/")]
 async fn index() -> impl Responder {
-    "Test"
+    "I love catz :3"
+}
+
+pub async fn update_craftsman(
+    craftman_id: web::Path<String>,
+    patch_request: web::Json<PatchRequest>,
+) -> HttpResponse {
+    let max_driving_distance = patch_request.max_driving_distance;
+    let profile_picture_score = patch_request.procile_picture_score;
+    let profile_description_score = patch_request.profile_description_score;
+
+    if max_driving_distance.is_none()
+        && profile_picture_score.is_none()
+        && profile_description_score.is_none()
+    {
+        return HttpResponse::BadRequest().json("Missing RequestBody-Data");
+    }
+
+    // Add sql magic here
+
+    HttpResponse::Ok().json(format!("Craftsman {} updated successfully", craftman_id))
 }
 
 #[get("/{post_code}")]
-async fn hello(post_code: web::Path<String>) -> impl Responder {
+async fn get_craftsmen_data(post_code: web::Path<String>) -> impl Responder {
+    // Add sql magic here
+
     format!("Hello {}!\n", &post_code)
 }
