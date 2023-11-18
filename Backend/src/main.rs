55fede12728d93e9265955cbada9047602223536
pvlov <ge84nun@mytum.de>
2023-11-18 16:09:06 +0100

    added some documentation

--- Backend/src/main.rs
+++ Backend/src/main.rs
@@ -1,17 +1,17 @@
 mod server;
+
 use actix_web::{web, App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
+
     println!("Listening on 127.0.0.1:8080");
     HttpServer::new(|| {
-        App::new()
-            .service(server::index)
-            .service(server::get_craftsmen_data)
-            .service(
-                web::resource("/craftman/{craftman_id}")
-                    .route(web::patch().to(server::update_craftsman)),
-            )
+        App::new().service(server::get_craftsmen_data).service(
+            web::resource("/craftman?{craftman_id}")
+                .route(web::patch().to(server::update_craftsman)),
+        )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
--- Backend/src/server.rs
+++ Backend/src/server.rs
@@ -1,16 +1,30 @@
 use actix_web::{get, web, HttpResponse, Responder};
 use serde::Deserialize;
 
+/// Struct representing a PATCH-Request
+///
+/// FIELDS:
+///     max_driving_distance = Maximum distance a craftsman is willing to drive
+///     profile_picture_score = Current Score of the craftsman's profile picture
+///     profile_description_score: Current Score of the craftsman's description
 #[derive(Deserialize)]
 pub struct PatchRequest {
     max_driving_distance: Option<i64>,
-    procile_picture_score: Option<i64>,
+    profile_picture_score: Option<i64>,
     profile_description_score: Option<i64>,
 }
 
-#[get("/")]
-async fn index() -> impl Responder {
-    "I love catz :3"
+/// Struct representing a Craftsmen
+///
+/// FIELDS:
+///
+///
+
+pub struct Craftsman {
+    id: u64,
+    first_name: String,
+    last_name: String,
+    distance: f64,
 }
 
 pub async fn update_craftsman(
@@ -18,15 +32,17 @@ pub async fn update_craftsman(
     patch_request: web::Json<PatchRequest>,
 ) -> HttpResponse {
     let max_driving_distance = patch_request.max_driving_distance;
-    let profile_picture_score = patch_request.procile_picture_score;
+    let profile_picture_score = patch_request.profile_picture_score;
     let profile_description_score = patch_request.profile_description_score;
 
     if max_driving_distance.is_none()
         && profile_picture_score.is_none()
         && profile_description_score.is_none()
     {
-        return HttpResponse::BadRequest().json("Missing RequestBody-Data");
+        return HttpResponse::BadRequest().json("Missing PatchRequest-Data");
     }
+
+
 
     // Add sql magic here
 
@@ -34,8 +50,17 @@ pub async fn update_craftsman(
 }
 
 #[get("/{post_code}")]
-async fn get_craftsmen_data(post_code: web::Path<String>) -> impl Responder {
+async fn get_craftsmen_data(_post_code: web::Path<String>) -> impl Responder {
     // Add sql magic here
 
-    format!("Hello {}!\n", &post_code)
+    let vec = vec!["Test-Data", "Test-Data", "Test-Data"];
+
+    HttpResponse::Ok().json(vec)
 }
+
+pub async fn compute_rank() -> i64 {
+    let distance = 0;
+
+    0
+}
+// background:#063773 check24 blue color
